d071847320134cf6b0c79b517e270f389aea0f99
amiame <amiame2008@gmail.com>
2020-05-19 15:46:21 +0900

    Debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ if [ ! -f "$(npm bin)/eslint" ]; then
 fi
 
 $(npm bin)/eslint --version
+echo "Filter mode: ${INPUT_FILTER_MODE}"
 
 if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
   # Use jq and github-pr-review reporter to format result to include link to rule page.
